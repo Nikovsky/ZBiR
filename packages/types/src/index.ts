862e7902c82de0a6ea30649a2ea56b026f9939e0
nikovsky <nfeduniewicz@gmail.com>
2025-06-25 00:23:58 +0200

    Refactor code structure for improved readability and maintainability

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,9 +1,32 @@
 // @file: packages/types/src/index.ts
-export { UserRole, Gender, SessionResponse } from './user';
-export { NotificationData, NotificationType } from './notification';
-export { PublicUserAccountDto, UpdateUserAccountDto } from './user-dashboard';
-export { APIMessageResponse } from './responses';
-export { SortDirection, PaginationQuery, PaginationResponse, DEFAULT_PAGINATION_LIMIT, ALLOWED_PAGINATION_LIMITS, AllowedPaginationLimits } from './pagination';
-export { GroupedUserSession, UserSession, GroupedUserSessionSortBy, UserSessionSortBy } from './admin-session';
-export { AdminPanelUserDto, AdminPanelPersonalDataDto, AdminPanelUserWithPersonalDataDto, AdminPanelUserSortFields, AdminPanelEditUserDto, AdminPanelEditPersonalDataDto, AdminPanelEditUserWithPersonalDataDto, AdminPanelCreateUserDto, AdminPanelPasswordChangeDto, AdminPanelDeleteUserDto } from './admin-user';
-export { AccessPreset, AccessMap } from './rbac';
+export { UserRole, Gender, SessionResponse } from "./user";
+export { NotificationData, NotificationType } from "./notification";
+export { PublicUserAccountDto, UpdateUserAccountDto } from "./user-dashboard";
+export { APIMessageResponse } from "./responses";
+export {
+  SortDirection,
+  PaginationQuery,
+  PaginationResponse,
+  DEFAULT_PAGINATION_LIMIT,
+  ALLOWED_PAGINATION_LIMITS,
+  AllowedPaginationLimits,
+} from "./pagination";
+export {
+  GroupedUserSession,
+  UserSession,
+  GroupedUserSessionSortBy,
+  UserSessionSortBy,
+} from "./admin-session";
+export {
+  AdminPanelUserDto,
+  AdminPanelPersonalDataDto,
+  AdminPanelUserWithPersonalDataDto,
+  AdminPanelUserSortFields,
+  AdminPanelEditUserDto,
+  AdminPanelEditPersonalDataDto,
+  AdminPanelEditUserWithPersonalDataDto,
+  AdminPanelCreateUserDto,
+  AdminPanelPasswordChangeDto,
+  AdminPanelDeleteUserDto,
+} from "./admin-user";
+export { AccessPreset, AccessMap } from "./rbac";
